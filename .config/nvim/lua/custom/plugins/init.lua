27a1e8477c29ed53a0cd558cd5104985d483ed1e
Youssef K <youssefmkachkache@gmail.com>
2024-03-31 00:51:07 +0000

    add a plugin to transparent toggle

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,19 +1,28 @@
 return {
-    {
-        'Exafunction/codeium.vim',
-        config = function ()
-          -- Change '<C-g>' here to any keycode you like.
-          vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-          vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-          vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-          vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-        end
-    },
-    {
-		"nvimdev/dashboard-nvim",
-		event = "VimEnter",
-		opts = function(_, opts)
-			local logo = [[
+  {
+    'Exafunction/codeium.vim',
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-g>', function()
+        return vim.fn['codeium#Accept']()
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function()
+        return vim.fn['codeium#CycleCompletions'](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function()
+        return vim.fn['codeium#CycleCompletions'](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function()
+        return vim.fn['codeium#Clear']()
+      end, { expr = true, silent = true })
+    end,
+  },
+
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    opts = function(_, opts)
+      local logo = [[
                 
          ██████╗ ████████╗██╗██████╗ ██╗██╗  ██╗        
          ██╔═══██╗╚══██╔══╝██║██╔══██╗██║╚██╗██╔╝        
@@ -24,8 +33,11 @@ return {
                                                          
  ]]
 
-			logo = string.rep("\n", 8) .. logo .. "\n\n"
-			opts.config.header = vim.split(logo, "\n")
-		end,
-	},
+      logo = string.rep('\n', 8) .. logo .. '\n\n'
+      opts.config.header = vim.split(logo, '\n')
+    end,
+  },
+
+  'xiyaowong/nvim-transparent',
 }
+
