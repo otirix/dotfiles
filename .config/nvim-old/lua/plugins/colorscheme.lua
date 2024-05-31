557c8d6b1118bace12d33cc6e19ef82ad860c34c
Youssef K <youssefmkachkache@gmail.com>
2024-05-31 03:38:09 +0100

    remove old nvim configs (kickstart.nvim)

--- .config/nvim-old/lua/plugins/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-return {
-    { "marko-cerovac/material.nvim",
-    priority = 1000,
-    config = function()
-        vim.g.material_style = "deep ocean"
-        vim.cmd.colorscheme("material")
-    end, },
-  }
