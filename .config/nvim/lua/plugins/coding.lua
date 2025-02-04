aed4eba352af5c2ba5ee5a97dd06e82b5ba46fd4
Youssef K <youssefmkachkache@gmail.com>
2025-02-04 21:46:49 +0100

    add new plugin for package.json and update lazy

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -177,4 +177,14 @@ return {
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
+
+  {
+    "vuki656/package-info.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("package-info").setup()
+    end,
+  },
 }
