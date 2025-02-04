9edcf2f0f3077171ef614901c8237025b1e7c7ae
Youssef K <youssefmkachkache@gmail.com>
2025-02-04 21:47:54 +0100

    customize fzf-lua action keymaps

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -28,6 +28,20 @@ return {
 
   {
     "ibhagwan/fzf-lua",
+    opts = function(_, opts)
+      local fzf = require("fzf-lua")
+      local actions = fzf.actions
+
+      return {
+        files = {
+          cwd_prompt = false,
+          actions = {
+            ["ctrl-i"] = { actions.toggle_ignore },
+            ["ctrl-h"] = { actions.toggle_hidden },
+          },
+        },
+      }
+    end,
   },
 
   {
