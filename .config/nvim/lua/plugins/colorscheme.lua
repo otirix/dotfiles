1db6656cead78c84e602599593b2ff38afaf3cb4
Youssef K <youssefmkachkache@gmail.com>
2025-09-15 08:58:09 +0100

    add bufferline workaround

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -46,6 +46,16 @@ return {
   },
 
   {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
+
+  {
     enabled = false,
     "rose-pine/neovim",
     name = "rose-pine",
