9783b3efa9d89f523de27627537d208ead44f844
Youssef K <youssefmkachkache@gmail.com>
2025-09-30 18:43:34 +0100

    update origins and delete old colorscheme workaround

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -46,16 +46,6 @@ return {
   },
 
   {
-    "akinsho/bufferline.nvim",
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
-
-  {
     enabled = false,
     "rose-pine/neovim",
     name = "rose-pine",
